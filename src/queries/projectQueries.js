2b94c000dae54aaa200b9b95af80f97e0f932fd9
Darren Bawag <renash12@gmail.com>
2023-01-31 11:32:36 +0800

    Update projectQueries.js

--- src/queries/projectQueries.js
+++ src/queries/projectQueries.js
@@ -1,31 +1,31 @@
 import { gql } from "@apollo/client"
 
 export const GET_PROJECTS = gql`
-  query getProjects {
-    projects {
-      id
-      name
-      status
-      client {
-        name
-      }
-    }
-  }
-`
+	query getProjects {
+		projects {
+			id
+			name
+			status
+			clientId {
+				name
+			}
+		}
+	}
+`;
 
 export const GET_PROJECT = gql`
-  query getProject($id: ID!) {
-    project(id: $id) {
-      id
-      name
-      description
-      status
-      client {
-        id
-        name
-        email
-        phone
-      }
-    }
-  }
-`
+	query getProject($id: ID!) {
+		project(id: $id) {
+			id
+			name
+			description
+			status
+			clientId {
+				id
+				name
+				email
+				phone
+			}
+		}
+	}
+`;
